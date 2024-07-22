4a952487dca24f2fd11a9e94286c24d89b1d183e
Nicolas Garcia <Rakultur@hotmail.com>
2024-07-22 13:55:38 -0500

    Modificaciones

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ function updatemenu() {
         document.getElementById('menu').style.borderRadius = '10px';
     }
 }
+
+// Slider
+let index = 0; // Current slide
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(n) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === n ? "block" : "none";
+    });
+}
+
+// Next/previous controls
+function changeSlides(n) {
+    index += n;
+    if (index >= slides.length) index = 0;
+    if (index < 0) index = slides.length - 1;
+    showSlide(index);
+}
+
+// Initial display
+showSlide(index);
